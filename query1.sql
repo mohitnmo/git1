db731c075e6d931d5125dce8b9f83a333a8abee4
Mohit Sharma <ms4684348@gmail.com>
2021-08-20 17:47:44 +0530

    new update

--- query1.sql
+++ query1.sql
@@ -1,5 +1,5 @@
 --drop table insaan5;
-create table if not exists insaan6 (id int primary key, name varchar2(10));
+--create table if not exists insaan6 (id int primary key, name varchar2(10));
 --insert into insaan6 values(1,'rahul');
 --insert into insaan6 values(2,'aman');
 --insert into insaan6 values(3,'rajesh');
@@ -7,4 +7,4 @@ create table if not exists insaan6 (id int primary key, name varchar2(10));
 --insert into insaan6 values(5,'sid');
 
 select * from insaan6;
-select id from insaan6 where id >2;
+select name from insaan6 where id>2;
